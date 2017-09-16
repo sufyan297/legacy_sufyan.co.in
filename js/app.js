c1466a17f79948cbd417e16f1c14c688e8c1e182
Mohammed Sufyan <sufyan297@live.com>
2017-09-16 17:38:09 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@
         console.log("Document Ready!");
         /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 
-        // particlesJS.load('particle', 'js/particlesjs-config.json', function() {
-        //   console.log('callback - particles.js config loaded');
-        // });
+        particlesJS.load('particle', 'js/particlesjs-config.json', function() {
+          console.log('callback - particles.js config loaded');
+        });
 
 
         var typed = new Typed('#anim-text', {
